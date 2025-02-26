6d0a10bb5c9ee8c6972b5d8c5a38d4d00f50bdb4
Alex Cline <rev.a.r.cline@gmail.com>
2025-02-26 14:36:53 -0600

    cruddy refactor that doesnt work

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c)
-                } else {
-                    println!("{}", c);
-                }
-                if c == 'q' {
-                    disable_raw_mode().unwrap();
-                    break;
-                }
-            }
-            Err(err) => println!("Error: {}", err),
-        }
-    }
-    disable_raw_mode().unwrap();
+    let editor = Editor::default();
+    editor.run();
 }
